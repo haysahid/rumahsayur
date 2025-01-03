3dc3d4392b054f72cbfb2cd64c6855dc6a7b19cf
Sahid Anwar <hay.sahid@gmail.com>
2025-01-04 04:58:29 +0700

    feat: setup tailwind css

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/products', 'Product::index');
+$routes->get('/products/(:num)', 'Product::show/$1');
